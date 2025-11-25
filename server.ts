fb8c1df8429fedecb1d2e65e4b539296b7a67120
arkhya <venkatnarayana@arkhyatech.net>
2025-11-25 12:11:53 +0530

    fixes

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { renderApplication } from '@angular/platform-server';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -12,7 +12,7 @@ export function app(): express.Express {
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine();
+  // SSR engine setup for Angular 20+
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -24,20 +24,18 @@ export function app(): express.Express {
     maxAge: '1y'
   }));
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+  // All regular routes use the Angular SSR engine
+  server.get('*', async (req, res, next) => {
+    try {
+      const html = await renderApplication(bootstrap, {
+        document: indexHtml,
+        url: req.originalUrl,
+        platformProviders: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+      });
+      res.send(html);
+    } catch (err) {
+      next(err);
+    }
   });
 
   return server;
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { withAppShell } from '@angular/ssr';
+import { AppShell } from './app-shell/app-shell';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+  provideServerRendering()
   ]
 };
 
