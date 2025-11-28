88f3bbcbbcfe887a381a45ed63e3ee24e77e8ed4
sundeep-one1 <sundeep.oneinfo@gmail.com>
2025-11-28 11:29:43 +0530

    Update environment configuration for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:3000',
-  production:false,
+ // apiBaseUrl: 'http://localhost:3000',
+//  production:false,
   // apiUrl: 'http://localhost:3000'
 
-  // production: false,
-  // apiBaseUrl: 'https://trendstarz-backend.onrender.com/api'
+  production: false,
+  apiBaseUrl: 'https://trendstarz-backend.onrender.com/api'
 
 };
