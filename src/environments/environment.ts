b7def776913be923a7ea072f212dee5b132441e1
arkhya <venkatnarayana@arkhyatech.net>
2025-12-10 21:53:29 +0530

    envi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:3000',
-  // production: true,
-  // apiBaseUrl: 'https://trendstarz-backend-production.up.railway.app',
+  // production: false,
+  // apiBaseUrl: 'http://localhost:3000',
+  production: true,
+  apiBaseUrl: 'https://trendstarz-backend-production.up.railway.app',
   stripePublicKey: 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXX' // <-- Replace with your real Stripe public key
 };
