440622e118e0e95c7240524e9d3507e2846cc4b6
arkhya <venkatnarayana@arkhyatech.net>
2026-02-25 17:20:07 +0530

    Api for backend to frontend fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://trendstarz-backend-production.up.railway.app',
+  apiBaseUrl: 'https://trendstarz-backend-production.up.railway.app/api',
   stripePublicKey: 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXX', // <-- Replace with your real Stripe public key
   cloudinaryUploadPreset: 'trendstarz_dev', // <-- Replace with your unsigned upload preset
   cloudinaryCloudName: 'ddnsoypf8' // <-- Replace with your cloud name
